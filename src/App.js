dbf143bfcbc34c4a80ef12184fb87c8cf9850b95
Попов Виталий <msk.vitaly@gmail.com>
2020-11-16 00:10:55 +0300

    Regirect to login

--- src/App.js
+++ src/App.js
@@ -2,36 +2,46 @@ import React from 'react'
 import { Layout } from 'antd'
 import './index.css'
 
-import { Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import { Redirect, Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import Navigation     from './components/Navigation.component'
 
 import CampaingsPage  from './pages/Campaigns'
 import OrdersPage     from './pages/Orders'
 import QuizzesPage    from './pages/Quizzes'
 import UsersPage      from './pages/Users'
+import Login          from '../src/components/auth/Login'
+import Register       from '../src/components/auth/Register'
 
 const { Header, Footer, Content } = Layout
 
 // Навигация & структура базовой страницы
 function App() {
+
+  let loggedIn = true;
+
   return (
   <Layout>
     <Router>
       <Header><Navigation/></Header>
       <Content>
-        <div style={{height: '75vh'}}>
+        <div style={{minHeight: '85vh'}}>
           <hr/>
           <Switch>
             <Route path="/campaings" component={CampaingsPage} />
             <Route path="/orders"    component={OrdersPage} />
             <Route path="/quizzes"   component={QuizzesPage} />
             <Route path="/users"     component={UsersPage} />
+            <Route path="/login"     component={Login} />
+            <Route path="/register"  component={Register} />
+
+            <Route exact path="/">
+              {loggedIn ? <Redirect to="/campaings" /> : <Redirect to="/login" /> }
+            </Route>
           </Switch>
           <hr/>
           </div>
       </Content>
     </Router>
-    <Footer><h4>Footer</h4></Footer>    
   </Layout>)
 }
 

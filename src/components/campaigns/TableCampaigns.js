c610e3f1015b748af4e960c1a995591ffc44027d
Kolbak <kushaevmarat@mail.ru>
2020-11-17 02:30:14 +0300

    chart statistic in campaigns page

--- src/components/campaigns/TableCampaigns.js
+++ src/components/campaigns/TableCampaigns.js
@@ -1,98 +1,121 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Table } from 'antd';
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Table } from "antd";
 
 function NCahootsCampaignsTable() {
-    const columns = [{
-        title: 'Name',
-        dataIndex: 'name',
-        filters: [
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      filters: [
         {
-            text: 'Joe',
-            value: 'Joe',
-        }, {
-            text: 'Jim',
-            value: 'Jim',
-        }, {
-            text: 'Submenu',
-            value: 'Submenu',
-            children: [
+          text: "Joe",
+          value: "Joe",
+        },
+        {
+          text: "Jim",
+          value: "Jim",
+        },
+        {
+          text: "Submenu",
+          value: "Submenu",
+          children: [
             {
-                text: 'Green',
-                value: 'Green',
-            }, {
-                text: 'Black',
-                value: 'Black',
+              text: "Green",
+              value: "Green",
             },
-            ],
-        },
-        ],
-        // specify the condition of filtering result
-        // here is that finding the name started with `value`
-        onFilter: (value, record) => record.name.indexOf(value) === 0,
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortDirections: ['descend'],
-        },
-        {
-            title: 'Age',
-            dataIndex: 'age',
-            defaultSortOrder: 'descend',
-            sorter: (a, b) => a.age - b.age,
-        },
-        {
-            title: 'Address',
-            dataIndex: 'address',
-            filters: [
             {
-                text: 'London',
-                value: 'London',
-            }, {
-                text: 'New York',
-                value: 'New York',
+              text: "Black",
+              value: "Black",
             },
-            ],
-            filterMultiple: false,
-            onFilter: (value, record) => record.address.indexOf(value) === 0,
-            sorter: (a, b) => a.address.length - b.address.length,
-            sortDirections: ['descend', 'ascend'],
+          ],
         },
-    ];
-      
-    const data = [
+      ],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.name.indexOf(value) === 0,
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ["descend", "ascend"],
+    },
+    {
+      title: "Employees",
+      dataIndex: "employees",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      filters: [
         {
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-        }, {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-        }, {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sidney No. 1 Lake Park',
-        }, {
-            key: '4',
-            name: 'Jim Red',
-            age: 32,
-            address: 'London No. 2 Lake Park',
-        }
-    ];
-      
-    function onChange(pagination, filters, sorter, extra) {
-        console.log('params', pagination, filters, sorter, extra);
-    }
-    
-    return (
-        <div>
-    <h3>Таблица</h3>
-    <Table columns={columns} dataSource={data} onChange={onChange} /></div>)
+          text: "London",
+          value: "London",
+        },
+        {
+          text: "New York",
+          value: "New York",
+        },
+      ],
+      filterMultiple: false,
+      onFilter: (value, record) => record.address.indexOf(value) === 0,
+      sorter: (a, b) => a.address.length - b.address.length,
+      sortDirections: ["descend", "ascend"],
+    },
+  ];
+
+  const data = [
+    {
+      key: "1",
+      name: "Company name 1",
+      employees: Math.round(Math.random() * 10000),
+      address: "New York No. 1 Lake Park",
+      description: "Details for 'Company name 1'",
+    },
+    {
+      key: "2",
+      name: "Company name 2",
+      employees: Math.round(Math.random() * 10000),
+      address: "London No. 1 Lake Park",
+      description: "Details for 'Company name 2'",
+    },
+    {
+      key: "3",
+      name: "Company name 3",
+      employees: Math.round(Math.random() * 10000),
+      address: "Sidney No. 1 Lake Park",
+      description: "Details for 'Company name 3'",
+    },
+    {
+      key: "4",
+      name: "Company name 4",
+      employees: Math.round(Math.random() * 10000),
+      address: "London No. 2 Lake Park",
+      description: "Details for 'Company name 4'",
+    },
+  ];
+
+  function onChange(pagination, filters, sorter, extra) {
+    console.log("params", pagination, filters, sorter, extra);
+  }
+
+  return (
+    <div>
+      <h3>Таблица</h3>
+      <Table
+        columns={columns}
+        dataSource={data}
+        onChange={onChange}
+        expandable={{
+          expandedRowRender: (record) => (
+            <p style={{ margin: 0 }}>{record.description}</p>
+          ),
+          rowExpandable: (record) => record.name !== "Not Expandable",
+        }}
+      />
+    </div>
+  );
 }
 
-NCahootsCampaignsTable.propTypes = {}
+NCahootsCampaignsTable.propTypes = {};
 
 export default NCahootsCampaignsTable;
--- src/pages/Campaigns.js
+++ src/pages/Campaigns.js
@@ -1,27 +1,43 @@
-import React from 'react'
-import TableNCahootsCampaigns  from '../components/campaigns/TableCampaigns'
-import { Layout } from 'antd'
+import React from "react";
+import { Layout, Card } from "antd";
 
-import PropTypes from 'prop-types'
+import "./Campaigns.css";
 
-const { Header, Sider, Content } = Layout
+import PropTypes from "prop-types";
 
+import TableNCahootsCampaigns from "../components/campaigns/TableCampaigns";
+import StatisticChart from "../components/campaigns/StatisticChart";
+
+const { Header, Sider, Content } = Layout;
 
 const CampaingsPage = () => {
-    return (
-        <Layout>
-            <Sider>Либо можно сделать статистику здесь</Sider>
-            <Layout>
-                <Header>Статистика</Header>
-                <Content>
-                    таблица с NCahoots Campaigns
-                    <TableNCahootsCampaigns/>
-                </Content>
-            </Layout>
-        </Layout>
-    )
-}
 
-CampaingsPage.propTypes = {}
+    // let [height, setHeight] = React.useState(null)
+  return (
+    <Layout>
+      <Sider width="30vw" height="100%">
+        {/* <Card
+          title="Статистика"
+          style={{ width: "100%", height: "100%" }}
+          className="chart-card"
+        > */}
+        <h2>Статистика</h2>
+          <div className="stat-chart-cont">
+            <StatisticChart />
+          </div>
+        {/* </Card> */}
+      </Sider>
+      <Layout>
+        <Header>Статистика</Header>
+        <Content>
+          таблица с NCahoots Campaigns
+          <TableNCahootsCampaigns />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 
-export default CampaingsPage
+CampaingsPage.propTypes = {};
+
+export default CampaingsPage;

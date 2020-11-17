1e95d92057f006e4c97361127761460b41cd43d3
Kolbak <kushaevmarat@mail.ru>
2020-11-17 07:46:27 +0300

    change layout in campaigns page | fix header

--- src/components/Navigation.component.js
+++ src/components/Navigation.component.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button } from "antd";
+
+import "./Navigation.component.css";
 
 const Navigation = () => (
-    <nav>
-        <div className="site-page-header-ghost-wrapper">
-        <PageHeader
-            ghost={true}
-            title="NCahoots"
-            extra={[
-            <Button key="1"><NavLink to="/campaings">NCahoots Campaigns </NavLink></Button>,
-            <Button key="2"><NavLink to="/orders">Orders                </NavLink></Button>,
-            <Button key="3"><NavLink to="/quizzes">Quizzes              </NavLink></Button>,
-            <Button key="4"><NavLink to="/users">Users                  </NavLink></Button>,
-            ]}
-            >
-        </PageHeader>
-        </div>            
-    </nav>
+  <nav className="page-nav">
+    <PageHeader
+      className="page-header"
+      ghost={true}
+      title="NCahoots"
+      extra={[
+        <Button key="1">
+          <NavLink to="/campaings">NCahoots Campaigns </NavLink>
+        </Button>,
+        <Button key="2">
+          <NavLink to="/orders">Orders </NavLink>
+        </Button>,
+        <Button key="3">
+          <NavLink to="/quizzes">Quizzes </NavLink>
+        </Button>,
+        <Button key="4">
+          <NavLink to="/users">Users </NavLink>
+        </Button>,
+      ]}
+    ></PageHeader>
+  </nav>
 );
 
 Navigation.propTypes = {};
-
 
 export default Navigation;
--- src/pages/Campaigns.js
+++ src/pages/Campaigns.js
@@ -11,26 +11,24 @@ import StatisticChart from "../components/campaigns/StatisticChart";
 const { Header, Sider, Content } = Layout;
 
 const CampaingsPage = () => {
-
-    // let [height, setHeight] = React.useState(null)
+  // let [height, setHeight] = React.useState(null)
   return (
-    <Layout>
-      <Sider width="30vw" height="100%">
-        {/* <Card
+    <Layout className="layout-main">
+      {/* <Sider width="30vw" height="100%"> */}
+      {/* <Card
           title="Статистика"
           style={{ width: "100%", height: "100%" }}
           className="chart-card"
         > */}
-        <h2>Статистика</h2>
-          <div className="stat-chart-cont">
-            <StatisticChart />
-          </div>
-        {/* </Card> */}
-      </Sider>
-      <Layout>
-        <Header>Статистика</Header>
+      <Layout className="layout-chart">
         <Content>
-          таблица с NCahoots Campaigns
+          <h2>Статистика</h2>
+          <StatisticChart />
+        </Content>
+      </Layout>
+      <Layout className="layout-table">
+        <Header>Таблица с NCahoots Campaigns</Header>
+        <Content>
           <TableNCahootsCampaigns />
         </Content>
       </Layout>
